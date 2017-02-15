214e446b1c993fff90e1195b7271f297bf78fd75
Brigette Eckert <Brigette.Eckert@live.com>
2017-02-14 23:38:44 -0800

    added event bindings, edit and click to to-do-app

--- Javascript/AngularJS/04-to-do-app/src/app/app.component.ts
+++ Javascript/AngularJS/04-to-do-app/src/app/app.component.ts
@@ -15,8 +15,20 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus Course', 'high'),
-    new Task('Brainstorm angular 2 project ideas', 'medium'),
-    new Task('Update Readme for Repos on github', 'low'),
+    new Task('Finish weekend Angular homework for Epicodus Course', 'High'),
+    new Task('Brainstorm angular 2 project ideas', 'Hedium'),
+    new Task('Update Readme for Repos on github', 'Low'),
   ];
+
+  editTask(){
+    alert("You just requested to edit a Task!")
+  }
+
+  isDone(clickedTask: Task){
+    if(clickedTask.done === true){
+      alert("The task is done!")
+    } else {
+      alert("The task is not done.  Better get to work!")
+    }
+  }
 }
