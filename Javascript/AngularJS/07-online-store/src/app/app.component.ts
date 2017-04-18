dbbcd4fd11cf689936607bafbf759d3ddeaa7671
Brigette Eckert <Brigette.Eckert@live.com>
2017-04-18 15:58:01 -0700

    setup up routing to display welcome compontent on home route on online-store project

--- Javascript/AngularJS/07-online-store/src/app/app.component.ts
+++ Javascript/AngularJS/07-online-store/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Cli is very good';
+  title = 'Epicodus Tunes';
 }
