2919ace76d19fbfad7a45e1ae39a7db097271d25
Brigette Eckert <Brigette.Eckert@live.com>
2017-04-28 10:45:53 -0700

    added about and marketplace compotents to online store finishing weekend work

--- Javascript/AngularJS/07-online-store/src/app/app.module.ts
+++ Javascript/AngularJS/07-online-store/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
