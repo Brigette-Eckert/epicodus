52c1ec9f66847e012f8429538e0675e9899cebe9
Brigette Eckert <brigette.eckert@live.com>
2018-01-24 14:13:20 -0800

    fixed bug in tamagotchi

--- Javascript/ReactJS/03-tamagotchi/src/App.js
+++ Javascript/ReactJS/03-tamagotchi/src/App.js
@@ -7,64 +7,66 @@ import './styles/App.css';
 
 //not sure if this is in right place - might need to put on egg lifecycle component
 const PETS = [{
-  name: "Bun",
-  species: "Bunny",
-  hatchDate: null,
+    name: "Bun",
+    species: "Bunny",
+    hatchDate: null,
 }, {
-  name: "Pusheen",
-  species: "Delightfully Chubby Cat",
-  hatchDate: null,
+    name: "Pusheen",
+    species: "Delightfully Chubby Cat",
+    hatchDate: null,
 }, {
-  name: "Biscuit",
-  species: "Corgi",
-  hatchDate: null,
+    name: "Biscuit",
+    species: "Corgi",
+    hatchDate: null,
 }];
 
 
 class App extends Component {
     //state active pets
-  constructor(props){
-    super(props);
-    this.state = JSON.parse(localStorage.getItem("appState")) || {
-      firstVisit: true,
-      activePet: null
-    };
-    this.setActivePet = this.setActivePet.bind(this);
-    this.resetPet = this.resetPet.bind(this);
-    this.syncState = this.syncState.bind(this);
-  }
+    constructor(props){
+        super(props);
+        this.state = JSON.parse(localStorage.getItem("appState")) || {
+            firstVisit: true,
+            activePet: null
+        };
+        this.setActivePet = this.setActivePet.bind(this);
+        this.resetPet = this.resetPet.bind(this);
+        this.syncState = this.syncState.bind(this);
+    }
 
-  setActivePet(activePet){
-    activePet.hatchDate = new moment().toString();
-    let newState = {
-      activePet: activePet,
-      firstVisit: false
-    };
+    setActivePet(activePet){
+        activePet.hatchDate = new moment().toString();
+        let newState = {
+            activePet: activePet,
+            firstVisit: false
+        };
 
-    this.setState(newState, this.syncState);
-  }
+        this.setState(newState, this.syncState);
+    }
 
-  syncState() {
-    localStorage.setItem("appState", JSON.stringify(this.state))
-  }
+    syncState() {
+        localStorage.setItem("appState", JSON.stringify(this.state))
+    }
 
-  resetPet(){
-    let newState = {
-      activePet: null,
-      firstVisit: true,
-    };
-    this.setState(newState, this.syncState);
-  }
+    resetPet(){
+        let newState = {
+            activePet: null,
+            firstVisit: true,
+        };
+        localStorage.clear();
+        console.log(this.state);
+        this.setState(newState, this.syncState);
+    }
 
-  render() {
-    let {firstVisit, activePet} = this.state;
-    return (
-      <div className="App">
-        { !!firstVisit && <EggContainer pets={PETS} setActivePet={this.setActivePet} /> }
-        { !firstVisit && <PetContainer pet={activePet} resetPet={this.resetPet}/> }
-      </div>
-    );
-  }
+    render() {
+        let {firstVisit, activePet} = this.state;
+        return (
+            <div className="App">
+                { !!firstVisit && <EggContainer pets={PETS} setActivePet={this.setActivePet} /> }
+                { !firstVisit && <PetContainer pet={activePet} resetPet={this.resetPet}/> }
+            </div>
+        );
+    }
 }
 
 export default App;
